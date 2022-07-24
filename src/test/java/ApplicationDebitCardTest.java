03da345a7b8f6a04f75cc2f43d2624821b1f50b9
Vladislav <vladislav_orlov_94@mail.ru>
2022-07-24 16:40:04 +0300

    update GUI test4

--- src/test/java/ApplicationDebitCardTest.java
+++ src/test/java/ApplicationDebitCardTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,8 +17,7 @@ public class ApplicationDebitCardTest {
 
     @BeforeAll
     static void setUpAll() {
-       System.setProperty("webdriver.chrome.driver", "/Users/vladislavorlov/Documents/Java Netology/auto home/autoHome2.1/driver/chromedriver");
-
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
