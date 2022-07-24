9952069db52d5daf83b361f304aef2b2fb4111b7
Vladislav <vladislav_orlov_94@mail.ru>
2022-07-24 15:46:20 +0300

    GUI tets

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/j4a8e259dc4922cj?svg=true)](https://ci.appveyor.com/project/Orlov94/autohome2-1)
---
+++ src/test/java/ApplicationDebitCard.java
@@ -0,0 +1,63 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ApplicationDebitCard {
+
+    private WebDriver driver;
+
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "/Users/vladislavorlov/Documents/Java Netology/auto home/autoHome2.1/driver/chromedriver");
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+
+    @Test
+    void newCardApplication(){
+        driver.get("http://localhost:9999");
+
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Михайлов Олег");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79253285723");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+    }
+    @Test
+    void newCardApplicationDoubleSurname(){
+        driver.get("http://localhost:9999");
+
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Ивано-Франк Константин");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+79253273026");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("paragraph")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+    }
+
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+}
