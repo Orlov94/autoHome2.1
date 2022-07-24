c2e90a3e1e287318d3a8205afef02a5fdccad9ed
Vladislav <vladislav_orlov_94@mail.ru>
2022-07-24 15:56:30 +0300

    update GUI tets1

--- src/test/java/ApplicationDebitCard.java
+++ src/test/java/ApplicationDebitCard.java
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.util.List;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
